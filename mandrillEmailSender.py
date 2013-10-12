ffbcf54e6c1ddab33233670bb1f23335169e3b25
Deepak Garg <deepakgarg.iitg@gmail.com>
2013-10-12 15:49:47 -0700

    changed the emailSender api

--- mandrillEmailSender.py
+++ mandrillEmailSender.py
@@ -6,7 +6,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-def sendEmails(subject, fromAddress, email_template, member_data):
+def sendEmails(subject, fromAddress, emailHtml, emailText, member_data):
     msg = MIMEMultipart('alternative')
     msg['Subject'] = subject
     msg['From']    = fromAddress 
@@ -19,18 +19,19 @@ def sendEmails(subject, fromAddress, email_template, member_data):
     s.login(username, password)
     
     for member in member_info:
-        context = {}
+#        context = {}
 #        for param in email_params:
 #            context[param] = member[param]
 #        text = html = env.from_string(email_template).render(context)    
-        text = html = email_template.replace('{{first_name}}', member['first'])
+        text = emailText.replace('{{first_name}}', member['first'])
+        html = emailHtml.replace('{{first_name}}', member['first'])
         part1 = MIMEText(text, 'plain')
         part2 = MIMEText(html, 'html')
         msg.attach(part1)
         msg.attach(part2)
         msg['To'] = member['email']
         s.sendmail(msg['From'], msg['To'], msg.as_string())
-        
+
     s.quit()
 
 
